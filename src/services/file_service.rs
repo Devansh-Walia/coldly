78f58ea52b965b242f1002f2317870f2c478a039
Devansh-Walia <devanshwalia9898@gmail.com>
2025-01-13 12:51:00 +0530

    implement get attachments fruition to get files from templates

--- src/services/file_service.rs
+++ src/services/file_service.rs
@@ -1,5 +1,7 @@
 use std::fs::File;
 use std::io::{self, Read};
+use std::fs;
+use std::path::PathBuf;
 
 pub fn read_file_to_string(path: &str) -> io::Result<String> {
     let mut file = File::open(path)?;
@@ -7,3 +9,19 @@ pub fn read_file_to_string(path: &str) -> io::Result<String> {
     file.read_to_string(&mut contents)?;
     Ok(contents)
 } 
+
+pub fn get_attachment(templates_path: &str) -> io::Result<Vec<PathBuf>> {
+    let pdf_files: Vec<PathBuf> = fs::read_dir(templates_path)?
+        .filter_map(|entry| {
+            let entry = entry.ok()?;
+            let path = entry.path();
+            if path.extension()? == "pdf" {
+                Some(path)
+            } else {
+                None
+            }
+        })
+        .collect();
+    
+    Ok(pdf_files)
+}
--- src/services/mod.rs
+++ src/services/mod.rs
@@ -5,3 +5,4 @@ pub mod file_service;
 pub use self::csv::process_csv;
 pub use self::email::EmailService; 
 pub use self::file_service::read_file_to_string;
+pub use self::file_service::get_attachment;
